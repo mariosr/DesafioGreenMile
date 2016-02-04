c4417558cf9667ece07e9754bdc30c5038c4beb2
mario falcao <mariofalcao.es@gmail.com>
2016-02-04 14:07:03 -0300

    insere digito

--- src/EncontraValores.java
+++ src/EncontraValores.java
@@ -1,18 +1,24 @@
+import java.util.Scanner;
+
 public class EncontraValores {
 
 	public static void main(String[] args) {
-
+		
+		// array de valores
 		int array[] = { 1, 3, 2, 7, 5 };
-
-		int s = 6;
-
+		
+		//Digite um valor para S
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Digite um valor para S: ");
+		int s = sc.nextInt();
+		
+		// complexidade O(n^2)
 		for (int i = 0; i < array.length; i++) {
 			for (int j = i; j < array.length; j++) {
 				int a = array[i];
 				int b = array[j];
 				if ((a + b) == s) {
-					System.out
-							.println("Array[" + i + "]" + " Array[" + j + "]");
+					System.out.println("Array[" + i + "]" + " Array[" + j + "]");
 				}
 			}
 		}
